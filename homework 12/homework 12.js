93109e020cd7706c520ad332e2724248c548b8bb
Valera Bedrickij <becrickijv@gmail.com>
2019-07-17 01:36:07 +0300

    Homework 12

--- homework 12/homework 12.js
+++ homework 12/homework 12.js
@@ -3,7 +3,7 @@ var arr = ['vasya', 'petya', 'vanya'];
 
 function objCreate(arr) {
     var newArr = [];
-    for (var i=0; i<arr.length; i++){
+    for (var i = 0; i < arr.length; i++) {
         var newObj = {};
         newObj.name = arr[i];
         newArr.push(newObj);
@@ -11,6 +11,7 @@ function objCreate(arr) {
     return newArr;
 
 }
+
 objCreate(arr);
 
 //Task 2
@@ -21,6 +22,7 @@ function clockCreate(arr) {
     return 'Текущее время : ' + arr.join(' : ');
 
 }
+
 clockCreate(arr);
 
 // Task 3
@@ -32,15 +34,16 @@ function vowels(text) {
     var count = 0;
     var textMod = text.toLowerCase().split('');
 
-    for(var i=0; i<textMod.length; i++) {
+    for (var i = 0; i < textMod.length; i++) {
         if (textMod[i] === 'а' || textMod[i] === 'е' || textMod[i] === 'ё' || textMod[i] === 'и' || textMod[i] === 'о' || textMod[i] === 'у' || textMod[i] === 'ы' || textMod[i] === 'э' || textMod[i] === 'ю' || textMod[i] === 'я') {
             count++
         }
 
     }
-    return  'Количество гласных букв в тексте: ' + count + '.';
+    return 'Количество гласных букв в тексте: ' + count + '.';
 
 }
+
 vowels(textInput);
 
 // Вариант через регулярное выражение
@@ -48,8 +51,9 @@ vowels(textInput);
 var textInput = 'Это было очень длинное лето';
 
 function vowels(text) {
-    return 'Количество гласных букв в тексте: ' + text.toLowerCase().replace(/[^аеёиоуыэюя]/gi,"").length + '.';
+    return 'Количество гласных букв в тексте: ' + text.toLowerCase().replace(/[^аеёиоуыэюя]/gi, "").length + '.';
 }
+
 vowels(textInput);
 
 // Вариант который вы возможно от нас хотите.
@@ -60,8 +64,8 @@ function vowels(text) {
     var count = 0;
     var textMod = text.toLowerCase().split('');
     var vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
-    for (var i =0; i <textMod.length; i++) {
-        for (var j = 0; j<vowels.length; j++) {
+    for (var i = 0; i < textMod.length; i++) {
+        for (var j = 0; j < vowels.length; j++) {
             if (textMod[i] === vowels[j]) {
                 count++
             }
@@ -69,6 +73,7 @@ function vowels(text) {
     }
     return count;
 }
+
 vowels(textInput);
 
 //Task 4
@@ -77,21 +82,22 @@ vowels(textInput);
 var textInput = "Это просто текст. Какое удивительное небо! Может по пиву? Нет, не сегодня";
 
 function editText(textInput) {
-    var textMod= textInput.toLowerCase().split(/[\!?.]/);
-    for (var i=0; i<textMod.length; i++) {
+    var textMod = textInput.toLowerCase().split(/[\!?.]/);
+    for (var i = 0; i < textMod.length; i++) {
         var currentText = textMod[i].split('');
-        var count =0;
+        var count = 0;
 
-        for (var j= 0; j< currentText.length; j++){
+        for (var j = 0; j < currentText.length; j++) {
 
             if (currentText[j] != ' ' && currentText[j] != ',') {
                 count++;
             }
 
         }
-        console.log('Предложение: /' + textMod[i] + '/ содержит ' + count + ' букв.' )
+        console.log('Предложение: /' + textMod[i] + '/ содержит ' + count + ' букв.')
     }
 
 }
+
 editText(textInput);
 
