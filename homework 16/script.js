52e358e709609a8cdf98e86727213d8dcdc8ba69
Valera Bedrickij <becrickijv@gmail.com>
2019-08-21 23:49:32 +0300

    Fix

--- homework 16/script.js
+++ homework 16/script.js
@@ -1,47 +1,51 @@
-var body = document.body;
-var mainButton = document.getElementById('mainButton');
-var additionalButtons = document.createElement('div');
-var saveContainer = document.createElement('div');
-var spanList = document.getElementsByTagName('span');
-var spanTenMins = spanList[0];
-var spanMins = spanList[1];
-var spanTenSecs = spanList[3];
-var spanSec = spanList[4];
-var spanTenMs = spanList[6];
-var spanMs = spanList[7];
-var tenMins = 0;
-var mins = 0;
-var tenSecs = 0;
-var secs = 0;
-var tenMs = 0;
-var ms = 0;
-var logic = 0;
-var i = 1;
-var saveButton;
+var body = document.body,
+    mainButton = document.getElementById('mainButton'),
+    additionalButtons = document.createElement('div'),
+    saveContainer = document.createElement('div'),
+    spanList = document.getElementsByTagName('span'),
+    spanTenMins = spanList[0],
+    spanMins = spanList[1],
+    spanTenSecs = spanList[3],
+    spanSec = spanList[4],
+    spanTenMs = spanList[6],
+    spanMs = spanList[7],
+    tenMins = 0,
+    mins = 0,
+    tenSecs = 0,
+    secs = 0,
+    tenMs = 0,
+    ms = 0,
+    logic = 0,
+    i = 1,
+    saveButton;
 
 mainButton.addEventListener('click', mainButtonState, false);
 
 function mainButtonState() {
-    if (mainButton.getAttribute('data-state') === 'start') {
-        logic = setInterval(mainLogic, 10);
-        additionalButtons.setAttribute('class', 'buttons');
-        additionalButtons.innerHTML = '<button id="reset">Reset</button><button id="save">Save</button>';
-        body.appendChild(additionalButtons);
-        saveContainer.setAttribute('class', 'save-container');
-        body.appendChild(saveContainer);
-        mainButton.setAttribute('data-state', 'pause');
-        mainButton.innerHTML = 'Pause';
 
-    } else if (mainButton.getAttribute('data-state') === 'pause') {
-        clearInterval(logic);
-        mainButton.setAttribute('data-state', 'resume');
-        mainButton.innerHTML = 'Resume';
-
-    } else if (mainButton.getAttribute('data-state') === 'resume') {
-        logic = setInterval(mainLogic, 10);
-        mainButton.setAttribute('data-state', 'pause');
-        mainButton.innerHTML = 'Pause';
+    switch (mainButton.getAttribute('data-state')) {
+        case 'start':
+            logic = setInterval(mainLogic, 10);
+            additionalButtons.setAttribute('class', 'buttons');
+            additionalButtons.innerHTML = '<button id="reset">Reset</button><button id="save">Save</button>';
+            body.appendChild(additionalButtons);
+            saveContainer.setAttribute('class', 'save-container');
+            body.appendChild(saveContainer);
+            mainButton.setAttribute('data-state', 'pause');
+            mainButton.innerHTML = 'Pause';
+            break;
+        case 'pause':
+            clearInterval(logic);
+            mainButton.setAttribute('data-state', 'resume');
+            mainButton.innerHTML = 'Resume';
+            break;
+        case 'resume':
+            logic = setInterval(mainLogic, 10);
+            mainButton.setAttribute('data-state', 'pause');
+            mainButton.innerHTML = 'Pause';
+            break;
     }
+
     saveButton = document.getElementById('save');
     var resetButton = document.getElementById('reset');
 
@@ -93,7 +97,7 @@ function mainLogic() {
             if (secs === 10) {
                 secs = 0;
                 tenSecs++;
-                if (tenSecs == 6) {
+                if (tenSecs === 6) {
                     tenSecs = 0;
                     mins++;
                     if (mins === 10) {
